2a19f7ef029610424d770c4f6105c6ba7b89fc60
alexeflorin <goremic@yahoo.com>
2023-05-13 00:36:36 -0400

    add some javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,28 @@
 
-var searchCity
-var apiKey
+var currentDate = dayjs().format('MMM D, YYYY, h:mm:ss a');
+
+var curentWeather
+var apiKey ="81e825ad1581e700cda654d2fba90839";
+// to get city names display
+var searchCity = function (cityNames){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid=${apisKey}')
+    .then(function (response) {
+        return response.json();
+      })
+}
+    .then(function(response){
+        var currentWeather=$("#current-weather-column");
+        currentWeather.addClass("current-weather-column");
+        currentCityName=$("#city-name");
+        currentDate=dayjs().format('MMM D, YYYY, h:mm:ss a');
+    }
+
+
+    )
+// city search
+var myString = ""; 
+var myNull = null;
+    var cityName=$("#search-city").val();
+    {
+
+    }
